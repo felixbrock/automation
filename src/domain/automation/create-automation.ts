894f51c00d37600e787b94f80740a3484bf26b1a
Felix Brockmeier <felix.brockmeier@outlook.com>
2021-08-06 15:43:27 +0100

    formatting

--- src/domain/automation/create-automation.ts
+++ src/domain/automation/create-automation.ts
@@ -3,9 +3,10 @@ import { v4 as uuidv4 } from 'uuid';
 import IUseCase from '../services/use-case';
 import Id from '../value-types/id';
 import { Automation, AutomationProperties } from '../entities/automation';
-import {buildAutomationDto, AutomationDto} from './automation';
-import {IAutomationRepository} from './i-automation-repository';
+import { buildAutomationDto, AutomationDto } from './automation';
+import { IAutomationRepository } from './i-automation-repository';
 import Result from '../value-types/transient-types/result';
+import { GetAccount, GetAccountResponseDto } from '../account-api/get-account';
 
 export interface CreateAutomationRequestDto {
   name: string;
@@ -15,29 +16,39 @@ export interface CreateAutomationRequestDto {
 export type CreateAutomationResponseDto = Result<AutomationDto | null>;
 
 export class CreateAutomation
-  implements
-    IUseCase<CreateAutomationRequestDto, CreateAutomationResponseDto>
+  implements IUseCase<CreateAutomationRequestDto, CreateAutomationResponseDto>
 {
   #automationRepository: IAutomationRepository;
 
-  public constructor(automationRepository: IAutomationRepository) {
+  #getAccount: GetAccount;
+
+  public constructor(
+    automationRepository: IAutomationRepository,
+    getAccount: GetAccount
+  ) {
     this.#automationRepository = automationRepository;
+    this.#getAccount = getAccount;
   }
 
   public async execute(
     request: CreateAutomationRequestDto
   ): Promise<CreateAutomationResponseDto> {
-    const automation: Result<Automation | null> =
-      this.#createAutomation(request);
-    if (!automation.value) return automation;
-
     try {
+      const accountIdValid = await this.#accountIdValid(request.accountId);
+
+      if (!accountIdValid)
+        throw new Error(
+          `No account for provided id ${request.accountId} found`
+        );
+
+      const automation: Result<Automation | null> =
+        this.#createAutomation(request);
+      if (!automation.value) return automation;
+
       // TODO Install error handling
       await this.#automationRepository.save(automation.value);
 
-      return Result.ok<AutomationDto>(
-        buildAutomationDto(automation.value)
-      );
+      return Result.ok<AutomationDto>(buildAutomationDto(automation.value));
     } catch (error) {
       return Result.fail<AutomationDto>(error.message);
     }
@@ -53,5 +64,19 @@ export class CreateAutomation
     };
 
     return Automation.create(automationProperties);
+  };
+
+  #accountIdValid = async (accountId: string): Promise<boolean> => {
+    try {
+      const getAccountResponse: GetAccountResponseDto =
+        await this.#getAccount.execute({ id: accountId });
+
+      if (getAccountResponse.error) return false;
+      if (!getAccountResponse.value) return false;
+
+      return true;
+    } catch (error) {
+      throw new Error(error);
+    }
   };
 }
